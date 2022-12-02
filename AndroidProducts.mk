00391ca18cfa2d163a0342cc974c372fc07d85a4
yoriscape <sukhpreetsingh58627@gmail.com>
2022-12-02 21:29:27 +0530

    voltageos bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,13 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
-# Copyright (C) 2022 VoidUI Project
+# Copyright (C) 2021-2022 VoltageOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_alioth.mk
+    $(LOCAL_DIR)/voltage_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_alioth-user \
-    aosp_alioth-userdebug \
-    aosp_alioth-eng
+    voltage_alioth-user \
+    voltage_alioth-userdebug \
+    voltage_alioth-eng
